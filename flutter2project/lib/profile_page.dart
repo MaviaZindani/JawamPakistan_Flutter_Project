17bddf53635656a53d42cb75591629f9c047ad24
MaviaZindani <maviazindaninfo@gmail.com>
2024-06-04 02:12:05 -0700

    first commit

---
+++ flutter2project/lib/profile_page.dart
@@ -0,0 +1,392 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> fruit = [
+    'mango',
+    'apple',
+    'pineapple',
+    'water melon',
+    'banana',
+    'aasafa',
+    'asfasffsaf',
+    'dfxcvcvxc',
+    'afsaffsa',
+    'afasfasfasfasdf',
+    'sfsafsfsfs',
+    'sfsdfsdfads',
+    'sddasgasdgasdfds',
+    'dsdfsdfsdfsdf',
+    'dsffsdfsdf'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+            ],
+          ),
+      
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+            ],
+          ),
+      
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.white,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
